857fe0ad940bba71e9c81ed276b9b21fb371f7b4
Kyle Lange <baronsintrees@gmail.com>
2017-05-08 12:57:01 -0700

    add an example of node'js's official event emitter.

---
+++ event-emitter/app-node.js
@@ -0,0 +1,29 @@
+/*Node.js's emitter is a robust version of the same thing!
+In fact: Look!  I can even use the same code exactly.*/
+
+const Emitter = require('events');
+const EmitterDone = require('events');
+
+const emtr = new Emitter();
+const emtrDone = new EmitterDone();
+
+emtr.on('greet', function() {
+  console.log('This is a greet emitter hello.');
+});
+
+emtr.on('greet', function() {
+  console.log('Another event emitter greeting!');
+});
+
+emtr.on('done', function() {
+  console.log('The Hellos are now done.');
+});
+
+emtrDone.on('test',() => {
+  console.log('I lied.  Here is an ES6 hello.');
+});
+
+console.log('Hello');
+emtr.emit('greet');
+emtr.emit('done');
+emtrDone.emit('test');
