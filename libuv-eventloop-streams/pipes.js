03945454262032faff9b77d60a5afab5ff8ed4bb
Kyle Lange <baronsintrees@gmail.com>
2017-05-09 15:47:49 -0700

    add pipe example that reads/prints and also reads/compresses/prints using pipe method chaining.

--- libuv-eventloop-streams/pipes.js
+++ libuv-eventloop-streams/pipes.js
@@ -1,13 +1,24 @@
 //an example of using pipes to make the stream run faster by reading/writing each chunck as it happens.
 
 const fs = require('fs');
+const zlib = require('zlib');
 
 let readable = fs.createReadStream(__dirname + '/stream.txt');
 
 let writable = fs.createWriteStream(__dirname + '/streamCopy.txt');
 
+let gzip = zlib.createGzip();
+let compressed = fs.createWriteStream(__dirname + '/stream.txt.gz');
+
 readable.pipe(writable);
-console.log('Info in stream.txt has been piped to streamCopy.txt.');
+
+//this method chaning below is a common practice for READABLE or dual R/W streams
+readable.pipe(gzip).pipe(compressed);
+
+console.log('Info in stream.txt has been piped to streamCopy.txt and stream.text.gz.');
+
+//REMEMBER the pipe will also return a destination.
+
 //the .pipe will essentiall do a more robust version of the code below:
 
 
