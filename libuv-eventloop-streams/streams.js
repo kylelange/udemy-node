9d936815bcd5451fa2b7cbce51db02b123995989
Kyle Lange <baronsintrees@gmail.com>
2017-05-09 12:20:33 -0700

    add createReadStream and createWriteStream to a js file to allow node to read from a .txt file and write TO a .txt file.

---
+++ libuv-eventloop-streams/streams.js
@@ -0,0 +1,21 @@
+//how to use a stream in node.js
+
+/* Why do you need this?  This is how async works - you only get chuncks of info at a time.
+That is why you can WATCH the movie as you download it!*/
+
+const fs = require('fs');
+
+let readable = fs.createReadStream(__dirname + '/stream.txt', {encoding: 'utf8', highWaterMark: 1024});
+
+let writable = fs.createWriteStream(__dirname + '/streamCopy.txt');
+
+//I can use the event emitter .on becasue of prototypal inheritance: Stream constr. includes Event constr.
+//REMEMBER: 'data' is the magicword the event emitter is listening for in node code, somewhere.
+readable.on('data', function(chunk) {
+  console.log(chunk.length);
+  /*when you put in the highWaterMark, you can see your text + spots where you hit agins the top end of your buffer.  Esentially, you see your CHUNKS!*/
+  console.log(chunk);
+  writable.write(chunk)
+});
+
+//I wonder what the default size of the buffer is?  Default: 64kb
