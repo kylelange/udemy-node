6daafd6d7098ed3776adb9b1dca5dd20cd6df7a0
Kyle Lange <baronsintrees@gmail.com>
2017-05-11 14:19:35 -0700

    add json, body parsing examples of middleware and querysrtings.

--- npm-express/app.js
+++ npm-express/app.js
@@ -1,9 +1,13 @@
 const moment = require('moment');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 //environment variables
 let port = process.env.PORT || 3000;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
 
 //middleware example for file downloads:
 //this will place all static files in a link tag you write in the .get('/') call
@@ -36,9 +40,19 @@ app.get('/', function(req, res) {
 //the :id means you can put anything after person/ and this route will get you there.
 //this is pattern matching and there can be multiple values: /person/:id/:shoes
 
-//express way
+//express way with an added querystring to display/move around the url
 app.get('/person/:id', function(req, res) {
   res.render('person', { ID: req.params.id, querystring: req.query.querystring });
+});
+
+app.post('/person', urlencodedParser, (req, res) => {
+  res.send('Thank You for your information!');
+  console.log(`${req.body.firstname} ${req.body.lastname}`);
+});
+
+app.post('/personjson', jsonParser, (req, res) => {
+  res.send('Thank you for the JSON data.');
+  console.log(`${req.body.firstname} ${req.body.lastname}`);
 });
 
 //pre-express way
