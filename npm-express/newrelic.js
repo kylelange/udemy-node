79ac6bbd2b9cc6a49ea5a492420cc61cb45fbd77
Kyle Lange <baronsintrees@gmail.com>
2017-05-14 19:35:51 -0700

    add newrelic package.

---
+++ npm-express/newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Udemy-node'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'fb0b3d00e38249cd351315e0377c5451e480de70',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
